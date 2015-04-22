0f36033779fae17c903530d604de39935bb2e1b5
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-22 12:13:03 -0700

    video 5

--- connect.php
+++ connect.php
@@ -1,13 +1,15 @@
 <?php
 
+
 $mysqli = new mysqli('localhost', 'root', 'root', 'to-do-login');
 if ($mysqli->connect_error) {
 	die("Connection Error(" . $mysqli->connect_errno . ')'
 	. $mysqli->connect_error);
 }
 else{
-	echo "Connection made";
 
+	echo "Connection made";
+.
 }
 
 $mysqli->close();
