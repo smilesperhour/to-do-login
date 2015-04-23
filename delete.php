a8dd099a945a86b14f2e330db0d3dc0ba77301df
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-23 12:13:20 -0700

    video 13

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+	$task_id = strip_tags($_POST['id']});
+	require('connect.php');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'project');
+
+	if($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+
+	}
+?>
