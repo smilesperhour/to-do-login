9741c6636cd031c73c5644fe78bf9fdfd4fd9fc7
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-23 08:05:51 -0700

    video 7

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@ if ($mysqli->connect_error) {
 else{
 
 	echo "Connection made";
-.
+
 }
 
 $mysqli->close();
--- index.php
+++ index.php
@@ -5,15 +5,26 @@
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
+
 	<div class="wrap">
+
 		<div class="task-list">
 			<ul>
+
 			  <?php require("includes/connect.php"); ?>
+
 			</ul>
+
 	</div>
+
 	<form class="add-new-task" autocomplete="off">
+
 	 <input type="text" name="new-task" placeholder="Add new item..."/>
+	 <script type="text/javascript"></script>
+
 	 </form>
+
 	 </div>
+
 </body>
 </html>
